fb9ced04b8d7d6df003d4f1beccae3f174a3e5b5
Thomas <tnegrault@lefigaro.fr>
2020-05-30 02:17:37 +0200

    Add blog link in footer

--- src/Components/Layout/Footer.js
+++ src/Components/Layout/Footer.js
@@ -3,6 +3,10 @@ import React from "react";
 export const Footer = () => {
   return (
     <div className="footer">
+      <h4 className={"blog"}>
+        Check out my blog: <br />
+        <a href={"https://mydevjourney.net/"}>MyDevJourney.net</a>
+      </h4>
       <div className="pure-u-1 u-sm-1-2">
         <p className="legal-copyright">
           {` © 2019 - ${new Date().getFullYear()} `}
@@ -14,6 +18,7 @@ export const Footer = () => {
             Thomas Negrault
           </a>
         </p>
+
         <p>
           Feel free to contact me at{" "}
           <a href="mailto:contact@devtools.best">contact@devtools.best</a>
